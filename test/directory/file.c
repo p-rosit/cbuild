72bff9a6bec9b59a0abb8bf861bdee537ce4c549
p-rosit <pontus.rosqvist@gmail.com>
2024-02-20 14:49:54 +0100

    change test project to showcase functionality

--- test/directory/file.c
+++ test/directory/file.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include "file.h"
 
 double func(int a, int b) {
     return sqrt(a * a + b * b);
--- test/main.c
+++ test/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "test.h"
+#include "directory/file.h"
 
 int main() {
     int a = 5;
