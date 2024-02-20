ec961966f66da4aa2f79dfa00a32450ca1121160
p-rosit <pontus.rosqvist@gmail.com>
2024-02-20 11:02:14 +0100

    complete file api

---
+++ file.c
@@ -0,0 +1,80 @@
+#include <string.h>
+#include "build.h"
+#include "file.h"
+
+bld_string extract_name(bld_dirent* file) {
+    bld_string str = new_string();
+    append_string(&str, file->d_name);
+    return str;
+}
+
+bld_file make_file(bld_file_type type, bld_path* path, bld_dirent* file) {
+    return (bld_file) {
+        .type = type,
+        .path = *path,
+        .name = extract_name(file),
+        .compiler = NULL,
+    };
+}
+
+bld_file make_header(bld_path* path, bld_dirent* file) {
+    bld_file header = make_file(BLD_HEADER, path, file);
+    return header;
+}
+
+bld_file make_impl(bld_path* path, bld_dirent* file) {
+    bld_file impl = make_file(BLD_IMPL, path, file);
+    return impl;
+}
+
+bld_file make_test(bld_path* path, bld_dirent* file) {
+    bld_file test = make_file(BLD_TEST, path, file);
+    return test;
+}
+
+void free_file(bld_file* file) {
+    free_path(&file->path);
+    free_string(&file->name);
+    free_compiler(file->compiler);
+}
+
+bld_files new_files() {
+    return (bld_files) {
+        .capacity = 0,
+        .size = 0,
+        .files = NULL,
+    };
+}
+
+void clear_files(bld_files* files) {
+    files->size = 0;
+}
+
+void free_files(bld_files* files) {
+    for (size_t i = 0; i < files->size; i++) {
+        free_file(&files->files[i]);
+    }
+    free(files->files);
+}
+
+void append_file(bld_files* files, bld_file file) {
+    bld_file* fs;
+    size_t capacity = files->capacity;
+
+    if (files->size >= files->capacity) {
+        capacity += (capacity / 2) + 2 * (capacity < 2);
+        
+        fs = malloc(capacity * sizeof(bld_file));
+        if (fs == NULL) {
+            log_fatal("Could not increase size of file array.");
+        }
+
+        memcpy(fs, files->files, files->size * sizeof(bld_file));
+        free(files->files);
+        
+        files->capacity = capacity;
+        files->files = fs;
+    }
+
+    files->files[files->size++] = file;
+}
--- file.h
+++ file.h
@@ -1,18 +1,40 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <dirent.h>
 #include "path.h"
+#include "compiler.h"
+
+typedef struct dirent bld_dirent;
 
 typedef enum bld_file_type {
-    INVALID,
-    IMPL,
-    HEADER,
-    TEST,
+    BLD_INVALID,
+    BLD_IMPL,
+    BLD_HEADER,
+    BLD_TEST,
 } bld_file_type;
 
 typedef struct bld_file {
     bld_file_type type;
     bld_path path;
+    bld_string name;
+    bld_compiler* compiler;
 } bld_file;
+
+typedef struct bld_files {
+    size_t capacity;
+    size_t size;
+    bld_file* files;
+} bld_files;
+
+bld_file    make_header(bld_path*, bld_dirent*);
+bld_file    make_impl(bld_path*, bld_dirent*);
+bld_file    make_test(bld_path*, bld_dirent*);
+void        free_file(bld_file*);
+
+bld_files   new_files();
+void        clear_files(bld_files*);
+void        free_files(bld_files*);
+void        append_file(bld_files*, bld_file);
 
 #endif
