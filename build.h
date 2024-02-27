76a5d62c9c2210c0964e65247b492623c6266e0f
p-rosit <pontus.rosqvist@gmail.com>
2024-02-27 18:19:21 +0100

    change log level

--- build.h
+++ build.h
@@ -3,9 +3,40 @@
 
 #include <stdio.h>
 
-#define log_fatal(...) do {printf("\033[0;31m[FATAL]\033[0m " __VA_ARGS__); printf("\n"); exit(1);} while (0)
-#define log_warn(...)  do {printf("\033[0;33m[ WARN]\033[0m " __VA_ARGS__); printf("\n");} while (0) 
-#define log_info(...)  do {printf("[ INFO] " __VA_ARGS__); printf("\n");} while (0) 
-#define log_debug(...) do {printf("\033[34m[DEBUG]\033[0m " __VA_ARGS__); printf("\n");} while (0) 
+#ifndef LOG_LEVEL
+    #define LOG_LEVEL (1)
+#endif
+
+#define log_fatal(...) \
+    do {\
+        if (LOG_LEVEL <= 3) { \
+            printf("\033[0;31m[FATAL]\033[0m " __VA_ARGS__);\
+            printf("\n"); exit(1);\
+        } \
+    } while (0)
+
+#define log_warn(...) \
+    do { \
+        if (LOG_LEVEL <= 2) { \
+            printf("\033[0;33m[ WARN]\033[0m " __VA_ARGS__);\
+            printf("\n");\
+        } \
+    } while (0) 
+
+#define log_info(...) \
+    do {\
+        if (LOG_LEVEL <= 1) { \
+            printf("[ INFO] " __VA_ARGS__); \
+            printf("\n");\
+        } \
+    } while (0) 
+
+#define log_debug(...) \
+    do {\
+        if (LOG_LEVEL <= 0) { \
+            printf("\033[34m[DEBUG]\033[0m " __VA_ARGS__); \
+            printf("\n");\
+        } \
+    } while (0) 
 
 #endif
