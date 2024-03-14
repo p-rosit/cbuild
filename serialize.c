b3fd9fd1b867c280c3b3e9ff0197dc0a8a8be596
p-rosit <pontus.rosqvist@gmail.com>
2024-03-14 08:16:36 +0100

    relfect change to bld_node and bld_file when serializing

--- serialize.c
+++ serialize.c
@@ -11,11 +11,11 @@ void serialize_files(FILE*, bld_files*, int);
 void serialize_file(FILE*, bld_file*, int);
 void serialize_file_type(FILE*, bld_file_type);
 void serialize_file_id(FILE*, bld_file_identifier);
+void serialize_file_symbols(FILE*, bld_set*, int);
+void serialize_file_includes(FILE*, bld_set*, int);
 
 void serialize_graph(FILE*, bld_graph*, int);
 void serialize_node(FILE*, bld_node*, int);
-void serialize_functions(FILE*, bld_funcs*, int);
-void serialize_includes(FILE*, bld_set*, int);
 void serialize_edges(FILE*, bld_edges*, int);
 
 void serialize_key(FILE*, char*, int);
@@ -40,12 +40,16 @@ void save_cache(bld_project* project) {
     }
 
     fprintf(cache, "{\n");
-    fprintf(cache, "  \"compiler\": ");
-    serialize_compiler(cache, &project->compiler, depth);
+    serialize_key(cache, "compiler", depth);
+    serialize_compiler(cache, &project->compiler, depth + 1);
     fprintf(cache, ",\n");
 
-    fprintf(cache, "  \"project_graph\": ");
-    serialize_graph(cache, &project->graph, depth);
+    serialize_key(cache, "files", depth);
+    serialize_files(cache, &project->files, depth + 1);
+    fprintf(cache, ",\n");
+
+    serialize_key(cache, "graph", depth);
+    serialize_graph(cache, &project->graph, depth + 1);
     fprintf(cache, "\n");
 
     fprintf(cache, "}\n");
@@ -57,20 +61,21 @@ void save_cache(bld_project* project) {
 void serialize_compiler(FILE* cache, bld_compiler* compiler, int depth) {
     fprintf(cache, "{\n");
     
-    fprintf(cache, "%*c\"type\": ", 2 * (depth + 1), ' ');
+    serialize_key(cache, "type", depth);
     serialize_compiler_type(cache, compiler->type);
     fprintf(cache, ",\n");
 
-    fprintf(cache, "%*c\"executable\": \"%s\"", 2 * (depth + 1), ' ', compiler->executable);
+    serialize_key(cache, "executable", depth);
+    fprintf(cache, "\"%s\"", compiler->executable);
 
     if (compiler->options.array.size > 0) {
         fprintf(cache, ",\n");
-        fprintf(cache, "%*c\"options\": ", 2 * (depth + 1), ' ');
+        serialize_key(cache, "options", depth);
         serialize_compiler_options(cache, &compiler->options, depth + 1);
     }
 
     fprintf(cache, "\n");
-    fprintf(cache, "%*c}", 2 * depth, ' ');
+    fprintf(cache, "%*c}", 2 * (depth - 1), ' ');
 }
 
 void serialize_compiler_type(FILE* cache, bld_compiler_type type) {
@@ -91,11 +96,11 @@ void serialize_compiler_options(FILE* cache, bld_options* options, int depth) {
     
     for (size_t i = 0; i < options->array.size; i++) {
         if (i > 0) {fprintf(cache, ",\n");}
-        fprintf(cache, "%*c\"%s\"", 2 * (depth + 1), ' ', flags[i]);
+        fprintf(cache, "%*c\"%s\"", 2 * depth, ' ', flags[i]);
     }
 
     fprintf(cache, "\n");
-    fprintf(cache, "%*c]", 2 * depth, ' ');
+    fprintf(cache, "%*c]", 2 * (depth - 1), ' ');
 }
 
 void serialize_files(FILE* cache, bld_files* files, int depth) {
@@ -110,36 +115,54 @@ void serialize_files(FILE* cache, bld_files* files, int depth) {
         } else {
             first = 0;
         }
-        fprintf(cache, "%*c", 2 * (depth + 1), ' ');
+        fprintf(cache, "%*c", 2 * depth, ' ');
         serialize_file(cache, file, depth + 1);
     }
     fprintf(cache, "\n");
-    fprintf(cache, "%*c]", 2 * depth, ' ');
+    fprintf(cache, "%*c]", 2 * (depth - 1), ' ');
 }
 
 void serialize_file(FILE* cache, bld_file* file, int depth) {
     fprintf(cache, "{\n");
 
-    fprintf(cache, "%*c\"type\": ", 2 * (depth + 1), ' ');
+    serialize_key(cache, "type", depth);
     serialize_file_type(cache, file->type);
     fprintf(cache, ",\n");
 
-    fprintf(cache, "%*c\"id\": ", 2 * (depth + 1), ' ');
+    serialize_key(cache, "id", depth);
     serialize_file_id(cache, file->identifier);
     fprintf(cache, ",\n");
 
-    fprintf(cache, "%*c\"hash\": %ju,\n", 2 * (depth + 1), ' ', file->identifier.hash);
+    serialize_key(cache, "hash", depth);
+    fprintf(cache, "%ju", file->identifier.hash);
+    fprintf(cache, ",\n");
 
-    fprintf(cache, "%*c\"name\": \"%s\"", 2 * (depth + 1), ' ', make_string(&file->name));
+    serialize_key(cache, "name", depth);
+    fprintf(cache, "\"%s\"", make_string(&file->name));
+    fprintf(cache, ",\n");
 
     if (file->compiler != NULL) {
-        fprintf(cache, ",\n");
-        fprintf(cache, "%*c\"compiler\": ", 2 * (depth + 1), ' ');
+        serialize_key(cache, "compiler", depth);
         serialize_compiler(cache, file->compiler, depth + 1);
+        fprintf(cache, ",\n");
+    }
+
+    serialize_key(cache, "includes", depth);
+    serialize_file_includes(cache, &file->includes, depth + 1);
+
+    if (file->type != BLD_HEADER) {
+        fprintf(cache, ",\n");
+
+        serialize_key(cache, "defined_symbols", depth);
+        serialize_file_symbols(cache, &file->defined_symbols, depth + 1);
+        fprintf(cache, ",\n");
+
+        serialize_key(cache, "undefined_symbols", depth);
+        serialize_file_symbols(cache, &file->undefined_symbols, depth + 1);
     }
 
     fprintf(cache, "\n");
-    fprintf(cache, "%*c}", 2 * depth, ' ');
+    fprintf(cache, "%*c}", 2 * (depth - 1), ' ');
 }
 
 void serialize_file_type(FILE* cache, bld_file_type type) {
@@ -161,6 +184,38 @@ void serialize_file_id(FILE* cache, bld_file_identifier id) {
     fprintf(cache, "%ju", id.id);
 }
 
+void serialize_file_symbols(FILE* cache, bld_set* symbols, int depth) {
+    int first = 1;
+    char** symbol;
+    bld_iter iter = bld_iter_set(symbols);
+
+    fprintf(cache, "[\n");
+    while (bld_set_next(&iter, (void**) &symbol)) {
+        if (!first) {
+            fprintf(cache, ",\n");
+        } else {
+            first = 0;
+        }
+        fprintf(cache, "%*c\"%s\"", 2 * depth, ' ', *symbol);
+    }
+    fprintf(cache, "\n%*c]", 2 * (depth - 1), ' ');
+}
+
+void serialize_file_includes(FILE* cache, bld_set* includes, int depth) {
+    int first = 1;
+    fprintf(cache, "[\n");
+    for (size_t i = 0; i < includes->capacity + includes->max_offset; i++) {
+        if (includes->offset[i] >= includes->max_offset) {continue;}
+        if (!first) {
+            fprintf(cache, ",\n");
+        } else {
+            first = 0;
+        }
+        fprintf(cache, "%*c%ju", 2 * depth, ' ', includes->hash[i]);
+    }
+    fprintf(cache, "\n%*c]", 2 * (depth - 1), ' ');
+}
+
 void serialize_graph(FILE* cache, bld_graph* graph, int depth) {
     int first = 1;
     bld_iter iter = bld_iter_set(&graph->nodes.set);
@@ -173,82 +228,32 @@ void serialize_graph(FILE* cache, bld_graph* graph, int depth) {
         } else {
             first = 0;
         }
-        fprintf(cache, "%*c", 2 * (depth + 1), ' ');
+        fprintf(cache, "%*c", 2 * depth, ' ');
         serialize_node(cache, node, depth + 1);
     }
 
     fprintf(cache, "\n");
-    fprintf(cache, "%*c]", 2 * depth, ' ');
+    fprintf(cache, "%*c]", 2 * (depth - 1), ' ');
 }
 
 void serialize_node(FILE* cache, bld_node* node, int depth) {
     fprintf(cache, "{\n");
 
-    fprintf(cache, "%*c\"file\": ", 2 * (depth + 1), ' ');
-    serialize_file(cache, node->file, depth + 1);
+    serialize_key(cache, "file", depth);
+    fprintf(cache, "%ju", node->file_id);
     fprintf(cache, ",\n");
 
-    fprintf(cache, "%*c\"defined\": ", 2 * (depth + 1), ' ');
-    serialize_functions(cache, &node->defined_funcs, depth + 1);
-    fprintf(cache, ",\n");
-
-    fprintf(cache, "%*c\"undefined\": ", 2 * (depth + 1), ' ');
-    serialize_functions(cache, &node->used_funcs, depth + 1);
-    fprintf(cache, ",\n");
-
-    fprintf(cache, "%*c\"includes\": ", 2 * (depth + 1), ' ');
-    serialize_includes(cache, &node->includes, depth + 1);
-    fprintf(cache, ",\n");
-
-    fprintf(cache, "%*c\"functions_from\": ", 2 * (depth + 1), ' ');
+    serialize_key(cache, "symbol_edges", depth);
     serialize_edges(cache, &node->functions_from, depth + 1);
     fprintf(cache, ",\n");
 
-    fprintf(cache, "%*c\"included_in\": ", 2 * (depth + 1), ' ');
+    serialize_key(cache, "include_edges", depth);
     serialize_edges(cache, &node->included_in, depth + 1);
     fprintf(cache, "\n");
 
-    fprintf(cache, "%*c}", 2 * depth, ' ');
-}
+    fprintf(cache, "%*c}", 2 * (depth - 1), ' ');
 
-void serialize_functions(FILE* cache, bld_funcs* funcs, int depth) {
-    int first = 1;
-    char** func_ptr = funcs->set.values;
-    fprintf(cache, "[\n");
-
-    func_ptr = funcs->set.values;
-    for (size_t i = 0; i < funcs->set.capacity + funcs->set.max_offset; i++) {
-        if (funcs->set.offset[i] >= funcs->set.max_offset) {continue;}
-        if (!first) {fprintf(cache, ",\n");}
-
-        fprintf(cache, "%*c\"%s\"", 2 * (depth + 1), ' ', func_ptr[i]);
-        first = 0;
-    }
-
-    fprintf(cache, "\n");
-    fprintf(cache, "%*c]", 2 * depth, ' ');
-}
-
-void serialize_includes(FILE* cache, bld_set* includes, int depth) {
-    int first = 1;
-    if (includes->size == 0) {
-        fprintf(cache, "[]");
-        return;
-    } else if (includes->size == 1) {
-        fprintf(cache, "[%ju]", includes->hash[0]);
-        return;
-    }
-    fprintf(cache, "[\n");
-
-    for (size_t i = 0; i < includes->capacity + includes->max_offset; i++) {
-        if (includes->offset[i] >= includes->max_offset) {continue;}
-        if (!first) {fprintf(cache, ",\n");};
-        fprintf(cache, "%*c%ju", 2 * (depth + 1), ' ', includes->hash[i]);
-        first = 0;
-    }
-
-    fprintf(cache, "\n");
-    fprintf(cache, "%*c]", 2 * depth, ' ');
+    (void)(node);
 }
 
 void serialize_edges(FILE* cache, bld_edges* edges, int depth) {
@@ -269,12 +274,12 @@ void serialize_edges(FILE* cache, bld_edges* edges, int depth) {
     iter = bld_iter_array(&edges->array);
     while (bld_array_next(&iter, (void**) &index)) {
         if (i > 0) {fprintf(cache, ",\n");};
-        fprintf(cache, "%*c%ju", 2 * (depth + 1), ' ', *index);
+        fprintf(cache, "%*c%ju", 2 * depth, ' ', *index);
         i++;
     }
 
     fprintf(cache, "\n");
-    fprintf(cache, "%*c]", 2 * depth, ' ');
+    fprintf(cache, "%*c]", 2 * (depth - 1), ' ');
 }
 
 void serialize_key(FILE* cache, char* key, int depth) {
