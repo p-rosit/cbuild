a254ff11f2588c850f0a9e47d70afd14b0f6420a
p-rosit <pontus.rosqvist@gmail.com>
2024-03-22 07:45:40 +0100

    rename bld_graph -> bld_dependency_graph

--- dependencies.h
+++ dependencies.h
@@ -1,29 +1,22 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include "container.h"
 #include "path.h"
 #include "file.h"
 
-typedef struct bld_search_info bld_search_info;
-typedef struct bld_node bld_node;
-
-struct bld_node {
-    uintmax_t file_id;
-    bld_array functions_from;
-    bld_array included_in;
-};
-
-typedef struct bld_graph {
+typedef struct bld_dependency_graph {
     bld_set* files;
-    bld_set nodes;
-} bld_graph;
+    bld_graph include_graph;
+    bld_graph symbol_graph;
+} bld_dependency_graph;
 
-bld_graph           graph_t_new(bld_set*);
-void                graph_t_free(bld_graph*);
+bld_dependency_graph dependency_graph_new(bld_set*);
+void        dependency_graph_free(bld_dependency_graph*);
 
-void                graph_generate(bld_graph*, bld_path*);
-bld_search_info*    graph_functions_from(bld_graph*, bld_file*);
-bld_search_info*    graph_includes_from(bld_graph*, bld_file*);
-int                 graph_next_file(bld_search_info*, bld_file**);
+void        dependency_graph_generate(bld_dependency_graph*, bld_path*);
+bld_iter    dependency_graph_functions_from(bld_dependency_graph*, bld_file*);
+bld_iter    dependency_graph_includes_from(bld_dependency_graph*, bld_file*);
+int         dependency_graph_next_file(bld_iter*, bld_file**);
 
 #endif
