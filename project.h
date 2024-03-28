acbebecd164521c05780e42f4d11d4c8c918123f
p-rosit <pontus.rosqvist@gmail.com>
2024-03-28 16:24:03 +0100

    separate the project into different struct to capture the different states it can be in

--- project.h
+++ project.h
@@ -9,31 +9,48 @@
 
 #define BLD_CACHE_NAME "cache.json"
 
-typedef struct bld_project bld_project;
-struct bld_project {
+typedef struct bld_project_cache {
+    int loaded;
+    int applied;
+    bld_compiler compiler;
+    bld_path root;
+    bld_set files;
+} bld_project_cache;
+
+typedef struct bld_project_base {
     bld_path root;
     bld_path build;
+    bld_compiler compiler;
+    bld_project_cache cache;
+} bld_project_base;
+
+typedef struct bld_forward_project {
+    bld_project_base base;
     bld_array extra_paths;
     bld_set ignore_paths;
-    uintmax_t main_file;
-    bld_compiler compiler;
-    bld_set files;
-    bld_set changed_files;
-    bld_dependency_graph graph;
-    bld_project* cache;
-};
+    bld_array file_names;
+    bld_array file_compilers;
+} bld_forward_project;
 
-bld_project project_new(bld_path, bld_compiler);
-void        project_free(bld_project*);
+typedef struct bld_project {
+    bld_project_base base;
+    uintmax_t main_file;
+    bld_set files;
+    bld_dependency_graph graph;
+} bld_project;
 
 bld_path    project_path_extract(int, char**);
-void        project_add_build(bld_project*, char*);
-void        project_add_path(bld_project*, char*);
-void        project_ignore_path(bld_project*, char*);
-void        project_load_cache(bld_project*, char*);
-void        project_save_cache(bld_project*);
+bld_forward_project project_new(bld_path, bld_compiler);
 
-void        project_set_main_file(bld_project*, char*);
-void        project_set_compiler(bld_project*, char*, bld_compiler);
+void        project_add_build(bld_forward_project*, char*);
+void        project_add_path(bld_forward_project*, char*);
+void        project_ignore_path(bld_forward_project*, char*);
+void        project_load_cache(bld_forward_project*, char*);
+void        project_set_main_file(bld_forward_project*, char*);
+void        project_set_compiler(bld_forward_project*, char*, bld_compiler);
+
+bld_project project_resolve(bld_forward_project*);
+void        project_save_cache(bld_project*);
+void        project_free(bld_project*);
 
 #endif
