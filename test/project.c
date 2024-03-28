a46fa6273b1bc387f6f10c86b86a39fd71f996fc
p-rosit <pontus.rosqvist@gmail.com>
2024-03-28 16:26:13 +0100

    what the new building might look like

--- test/project.c
+++ test/project.c
@@ -2,49 +2,80 @@
 
 int main(int argc, char** argv) {
     int result = 0;
-    bld_project project = project_new(
-        project_path_extract(argc, argv),
-        compiler_new(BLD_GCC, "/usr/bin/gcc")
+    bld_compiler project_compiler;
+    bld_forward_project forward_project;
+    bld_project project;
+
+    project_compiler = compiler_new(BLD_GCC, "/usr/bin/gcc");
+    compiler_add_flag(&project_compiler, "-lm");
+    compiler_add_flag(&project_compiler, "-fsanitize=address");
+    compiler_add_flag(&project_compiler, "-std=c99");
+    compiler_add_flag(&project_compiler, "-Wall");
+    compiler_add_flag(&project_compiler, "-Wextra");
+    compiler_add_flag(&project_compiler, "-pedantic");
+
+    forward_project = project_new(project_path_extract(argc, argv), project_compiler);
+
+    project_add_build(&forward_project, "..");
+    rebuild_builder(&forward_project, argc, argv);
+
+    /* Optional */
+    project_load_cache(&forward_project, ".build_cache");
+
+    /* Optional, add specific compiler to any files in project */
+    project_set_compiler(
+        &forward_project, "file.c",
+        compiler_with_flags(
+            BLD_CLANG, "/usr/bin/clang",
+            "-Weverything", NULL
+        )
     );
 
-    project_add_build(&project, "..");
-    rebuild_builder(&project, argc, argv);
+    /* Mandatory */
+    project_set_main_file(&forward_project, "main.c");
 
-    compiler_add_flag(&project.compiler, "-lm");
-    compiler_add_flag(&project.compiler, "-fsanitize=address");
-    compiler_add_flag(&project.compiler, "-std=c99");
-    compiler_add_flag(&project.compiler, "-Wall");
-    compiler_add_flag(&project.compiler, "-Wextra");
-    compiler_add_flag(&project.compiler, "-pedantic");
+    /* Mandatory */
+    project = project_resolve(&forward_project);
 
-    bld_compiler cc = compiler_new(BLD_CLANG, "/usr/bin/clang");
-    compiler_add_flag(&cc, "-Weverything");
-    {
-
-        /* Optional */
-        project_load_cache(&project, ".build_cache");
-
-        /* Mandatory */
-        incremental_index_project(&project);
-
-        /* Optional, mandatory if main file set */
-        // generate_dependency_graph(&project);
-
-        /* Optional */
-        project_set_compiler(&project, "file.c", cc);
-
-        /* Optional */
-        project_set_main_file(&project, "main.c");
-        /* Mandatory */
-        result = incremental_compile_project(&project, "a.out");
-        if (result > 0) {
-            log_warn("Could not compile project");
-        } else if (result < 0) {
-            log_info("Entire project existed in cache");
-        }
-
-        project_save_cache(&project);
+    /* Mandatory */
+    result = incremental_compile_project(&project, "a.out");
+    if (result > 0) {
+        log_warn("Could not compile project");
+    } else if (result < 0) {
+        log_info("Entire project existed in cache");
     }
+
+    /* Optional */
+    project_save_cache(&project);
+
+    /* Mandatory */
+    project_free(&project);
+
+
+
+
+
+
+
+
+
+
+    /* Mandatory */
+    incremental_index_project(&project);
+
+    /* Optional, mandatory if main file set */
+    // generate_dependency_graph(&project);
+
+    /* Optional */
+    /* Mandatory */
+    result = incremental_compile_project(&project, "a.out");
+    if (result > 0) {
+        log_warn("Could not compile project");
+    } else if (result < 0) {
+        log_info("Entire project existed in cache");
+    }
+
+    project_save_cache(&project);
 
     project_free(&project);
     return 0;
