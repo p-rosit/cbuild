95d7e30f593a4abea5fade9939306a89e1467f0f
p-rosit <pontus.rosqvist@gmail.com>
2024-04-03 21:29:16 +0200

    remove generic json parse from parse.c

--- parse.c
+++ parse.c
@@ -6,8 +6,7 @@
 #include "logging.h"
 #include "path.h"
 #include "project.h"
-
-typedef int (*bld_parse_func)(FILE*, void*);
+#include "json.h"
 
 void ensure_directory_exists(bld_path*);
 int parse_cache(bld_project_cache*, bld_path*);
@@ -44,10 +43,6 @@ int parse_string(FILE*, bld_string*);
 int parse_uintmax(FILE*, uintmax_t*);
 int parse_uintmax_array(FILE*, bld_array*);
 int parse_uintmax_set(FILE*, bld_set*);
-
-int parse_array(FILE*, void*, bld_parse_func);
-int parse_map(FILE*, void*, int, int*, char**, bld_parse_func*);
-int next_character(FILE*);
 
 void ensure_directory_exists(bld_path* directory_path) {
     errno = 0;
@@ -672,154 +667,4 @@ int parse_uintmax(FILE* file, uintmax_t* num_ptr) {
 
     *num_ptr = num;
     return 0;
-}
-
-int next_character(FILE* file) {
-    int c = getc(file);
-    while (c != EOF && isspace(c)) {c = getc(file);}
-    return c;
-}
-
-int parse_array(FILE* file, void* obj, bld_parse_func parse_func) {
-    int value_num = 0;
-    int result, parse_complete;
-    int c;
-
-    parse_complete = 0;
-    c = next_character(file);
-    if (c == EOF) {log_warn("Unexpected EOF"); goto parse_failed;}
-    if (c != '[') {log_warn("Unexpected starting character: \'%c\'", c); goto parse_failed;}
-
-    c = next_character(file);
-    if (c == EOF) {log_warn("Unexpected EOF"); goto parse_failed;}
-    if (c == ']') {
-        parse_complete = 1;
-    } else {
-        ungetc(c, file);
-    }
-
-    while (!parse_complete) {
-        value_num += 1;
-        result = parse_func(file, obj);
-        if (result) {goto parse_failed;}
-        
-        c = next_character(file);
-        switch (c) {
-            case (']'): {
-                parse_complete = 1;
-            } break;
-            case (','): {
-                continue;
-            } break;
-            case (EOF): {
-                log_warn("Unexpected EOF");
-                goto parse_failed;
-            } break;
-            default: {
-                log_warn("Unexpected character, got \'%c\'", c);
-                goto parse_failed;
-            } break;
-        }
-    }
-
-    return value_num;
-    parse_failed:
-    return -1;
-}
-
-int parse_map(FILE* file, void* obj, int entries, int* parsed, char** keys, bld_parse_func* parse_funcs) {
-    int exists, index, key_num = 0;
-    int result, parse_complete;
-    bld_string str;
-    char c, *temp;
-
-    memset(parsed, 0, entries * sizeof(int));
-
-    parse_complete = 0;
-    c = next_character(file);
-    if (c == EOF) {log_warn("Unexpected EOF"); goto parse_failed;}
-    if (c != '{') {log_warn("Unexpected starting character: \'%c\'", c); goto parse_failed;}
-
-    c = next_character(file);
-    if (c == EOF) {log_warn("Unexpected EOF"); goto parse_failed;}
-    if (c == '}') {
-        parse_complete = 1;
-    } else {
-        ungetc(c, file);
-    }
-
-    while (!parse_complete) {
-        int i;
-
-        key_num += 1;
-        str = string_new();
-        result = parse_string(file, &str);
-        if (result) {
-            log_warn("Key %d could not be parsed, expected: [", key_num);
-            for (i = 0; i < entries; i++) {
-                if (i > 0) {printf(",\n");}
-                printf("  \"%s\"", keys[i]);
-            }
-            printf("\n]\n");
-            goto parse_failed;
-        }
-
-        exists = 0;
-        temp = string_unpack(&str);
-        for (i = 0; i < entries; i++) {
-            if (strcmp(temp, keys[i]) == 0) {
-                exists = 1;
-                index = i;
-            }
-        }
-
-        if (!exists) {
-            log_warn("\"%s\" is not a valid key, expected: [", temp);
-            for (i = 0; i < entries; i++) {
-                if (i > 0) {printf(",\n");}
-                printf("  \"%s\"", keys[i]);
-            }
-            printf("\n]\n");
-            goto parse_failed;
-        }
-        if (parsed[index]) {
-            log_warn("Duplicate key \"%s\" encountered", keys[index]);
-            goto parse_failed;
-        }
-
-        c = next_character(file);
-        if (c != ':') {
-            log_warn("Expected \':\', got \'%c\'", c);
-            goto parse_failed;
-        }
-
-        result = parse_funcs[index](file, obj);
-        if (result) {goto parse_failed;}
-        parsed[index] = 1;
-
-        c = next_character(file);
-        switch (c) {
-            case ('}'): {
-                string_free(&str);
-                parse_complete = 1;
-            } break;
-            case (','): {
-                string_free(&str);
-                continue;
-            } break;
-            case (EOF): {
-                log_warn("Unexpected EOF");
-                goto parse_failed;
-            } break;
-            default: {
-                log_warn("Unexpected character, got \'%c\'", c);
-                goto parse_failed;
-            } break;
-        }
-    }
-
-    return key_num;
-    parse_failed:
-    string_free(&str);
-    return -1;
 }
