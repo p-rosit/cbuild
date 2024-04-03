8a384917432744aa17926406bab1eced4bfa74a0
p-rosit <pontus.rosqvist@gmail.com>
2024-04-03 21:28:12 +0200

    move map and array parsing to json.c

---
+++ json.h
@@ -0,0 +1,16 @@
+#ifndef JSON_H
+#include <stdio.h>
+#include "container.h"
+
+typedef void (*bld_serialize_func)(FILE*, void*);
+typedef int (*bld_parse_func)(FILE*, void*);
+
+void json_serialize_array(FILE*, bld_iter, bld_serialize_func);
+void json_serialize_map(FILE*, void*, int, char**, bld_serialize_func*);
+
+int parse_array(FILE*, void*, bld_parse_func);
+int parse_map(FILE*, void*, int, int*, char**, bld_parse_func*);
+
+int next_character(FILE*);
+
+#endif
