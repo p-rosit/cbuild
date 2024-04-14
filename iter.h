b27194785771a8223fdcf435cbfdbb4da86359f3
p-rosit <pontus.rosqvist@gmail.com>
2024-04-14 12:17:40 +0200

    move iteration function prototypes to header

---
+++ iter.h
@@ -0,0 +1,56 @@
+#ifndef ITER_H
+#define ITER_H
+#include "array.h"
+#include "set.h"
+#include "graph.h"
+
+enum bld_container_type {
+    BLD_ARRAY,
+    BLD_SET,
+    BLD_TREE,
+    BLD_GRAPH
+};
+
+enum bld_graph_search_type {
+    BLD_GRAPH_DONE,
+    BLD_DFS
+};
+
+typedef struct bld_iter_graph {
+    enum bld_graph_search_type type;
+    const bld_graph* graph;
+    bld_array stack;
+    bld_set visited;
+} bld_iter_graph;
+
+
+typedef struct bld_iter_array {
+    const bld_array* array;
+    size_t index;
+} bld_iter_array;
+
+typedef struct bld_iter_set {
+    const bld_set* set;
+    size_t index;
+} bld_iter_set;
+
+union bld_iter_container {
+    bld_iter_array array_iter;
+    bld_iter_set set_iter;
+    bld_iter_graph graph_iter;
+};
+
+typedef struct bld_iter {
+    enum bld_container_type type;
+    union bld_iter_container as;
+} bld_iter;
+
+bld_iter    iter_array(const bld_array*);
+bld_iter    iter_set(const bld_set*);
+bld_iter    iter_graph(const bld_graph*, uintmax_t);
+int         iter_next(bld_iter*, void**);
+int         array_next(bld_iter_array*, void**);
+int         set_next(bld_iter_set*, void**);
+int         graph_next(bld_iter_graph*, uintmax_t*);
+
+#endif
