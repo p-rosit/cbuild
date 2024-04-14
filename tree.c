d028f128777f240939ee8bdc753f797a5c043714
p-rosit <pontus.rosqvist@gmail.com>
2024-04-14 12:16:40 +0200

    push nodes to tree

--- tree.c
+++ tree.c
@@ -14,3 +14,16 @@ void tree_free(bld_tree* tree) {
     array_free(&tree->data);
 }
 
+bld_tree_node tree_push(bld_tree* tree, bld_tree_node* parent, void* value) {
+    bld_tree_node node;
+
+    node.parent = parent->self;
+    node.self = parent->self + parent->size;
+    node.next = node.self;
+    node.size = 0;
+
+    array_insert(&tree->nodes, node.self, &node);
+    array_insert(&tree->data, node.self, value);
+
+    return node;
+}
--- tree.h
+++ tree.h
@@ -18,5 +18,6 @@ typedef struct bld_tree {
 
 bld_tree        tree_new(size_t);
 void            tree_free(bld_tree*);
+bld_tree_node   tree_push(bld_tree*, bld_tree_node*, void*);
 
 #endif
