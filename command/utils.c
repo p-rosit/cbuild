09ad697829c405cd9cb65c6ef8203d0c4fa5d9c4
p-rosit <pontus.rosqvist@gmail.com>
2024-05-03 17:49:22 +0200

    extract project meta data

--- command/utils.c
+++ command/utils.c
@@ -1,11 +1,72 @@
-#include "../bit_core/os.h"
 #include "../bit_core/logging.h"
+#include "../bit_core/os.h"
+#include "../bit_core/iter.h"
 #include "utils.h"
 
-const bld_string bld_build_path = STRING_COMPILE_TIME_PACK(".bit");
-const bld_string bld_target_path = STRING_COMPILE_TIME_PACK("target");
+const bld_string bld_path_build = STRING_COMPILE_TIME_PACK(".bit");
+const bld_string bld_path_target = STRING_COMPILE_TIME_PACK("target");
 
-bld_path bld_find_root(void) {
+bld_path data_find_root(void);
+bld_set data_find_targets(bld_path*);
+
+bld_data data_extract(void) {
+    bld_data data;
+
+    data.root = data_find_root();
+    data.targets = data_find_targets(&data.root);
+
+    return data;
+}
+
+void data_free(bld_data* data) {
+    bld_string* target;
+    bld_iter iter;
+
+    path_free(&data->root);
+
+    iter = iter_set(&data->targets);
+    while (iter_next(&iter, (void**) &target)) {
+        string_free(target);
+    }
+    set_free(&data->targets);
+}
+
+bld_set data_find_targets(bld_path* root) {
+    char* file_name;
+    bld_path cpy;
+    bld_set targets;
+    bld_os_dir* dir;
+    bld_os_file* file;
+
+    cpy = path_copy(root);
+    path_append_string(&cpy, string_unpack(&bld_path_build));
+    path_append_string(&cpy, string_unpack(&bld_path_target));
+
+    targets = set_new(sizeof(bld_string));
+    dir = os_dir_open(path_to_string(&cpy));
+    if (dir == NULL) {
+        path_free(&cpy);
+        return targets;
+    }
+
+    while ((file = os_dir_read(dir)) != NULL) {
+        bld_string name;
+
+        file_name = os_file_name(file);
+        if (file_name[0] == '.') {continue;}
+
+        name = string_pack(file_name);
+        name = string_copy(&name);
+
+        set_add(&targets, string_hash(file_name), &name);
+    }
+
+    os_dir_close(dir);
+    path_free(&cpy);
+    return targets;
+}
+
+bld_path data_find_root(void) {
     int root_found = 0;
     char cwd[FILENAME_MAX];
     bld_path root;
@@ -16,7 +77,7 @@ bld_path bld_find_root(void) {
     do {
         bld_os_dir* dir;
         bld_path temp = path_copy(&root);
-        path_append_string(&temp, string_unpack(&bld_build_path));
+        path_append_string(&temp, string_unpack(&bld_path_build));
 
         dir = os_dir_open(path_to_string(&temp));
         path_free(&temp);
--- command/utils.h
+++ command/utils.h
@@ -1,11 +1,18 @@
 #ifndef APPLICATION_UTILS_H
 #define APPLICATION_UTILS_H
 
+#include "../bit_core/set.h"
 #include "../bit_core/path.h"
 
-extern const bld_string bld_build_path;
-extern const bld_string bld_target_path;
+extern const bld_string bld_path_build;
+extern const bld_string bld_path_target;
 
-bld_path bld_find_root(void);
+typedef struct bld_data {
+    bld_path root;
+    bld_set targets;
+} bld_data;
+
+bld_data    data_extract(void);
+void        data_free(bld_data*);
 
 #endif
