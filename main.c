2f4b157b619bf6c24a31a06f85faa40968ccc712
p-rosit <pontus.rosqvist@gmail.com>
2024-05-03 17:52:12 +0200

    do less twiddling in main

--- main.c
+++ main.c
@@ -1,43 +1,23 @@
 #include "bit_core/args.h"
 #include "bit_core/logging.h"
 #include "bit_core/path.h"
-#include "command/utils.h"
-#include "command/init.h"
-#include "command/remove.h"
-
+#include "command/command.h"
 
 int main(int argc, char** argv) {
     int result;
-    bld_string str;
-    bld_path build_root;
     bld_args args;
+    bld_data data;
+    bld_command cmd;
 
     args = args_new(argc, argv);
     args_advance(&args); /* Ignore how program was invoked */
 
-    if (args_empty(&args)) {
-        log_error("Incorrect usage");
-        return -1;
-    }
+    cmd = command_parse(&args);
+    data = data_extract();
 
-    str = args_advance(&args);
-    build_root = bld_find_root();
-    if (build_root.str.size == 0 && !string_eq(&str, &bld_command_init)) {
-        log_error("No build file");
-        path_free(&build_root);
-        return -1;
-    }
-    log_debug("Root is: \"%s\"", path_to_string(&build_root));
+    result = command_execute(&cmd, &data);
 
-    if (string_eq(&str, &bld_command_init)) {
-        result = bld_init(&build_root, &args);
-    } else if (string_eq(&str, &bld_command_remove)) {
-        result = bld_remove(&build_root, &args);
-    } else {
-        log_info("bit: '%s' is not a bit command", string_unpack(&str));
-        result = -1;
-    }
-
-    path_free(&build_root);
+    command_free(&cmd);
+    data_free(&data);
     return result;
 }
