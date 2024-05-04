bc336f042f9d00098affde5963b4406849b3188d
p-rosit <pontus.rosqvist@gmail.com>
2024-05-04 14:24:49 +0200

    parse target config before executing build command

--- command/command.c
+++ command/command.c
@@ -1,4 +1,5 @@
 #include "../bld_core/logging.h"
+#include "../config/config_target.h"
 #include "command.h"
 
 bld_command command_parse(bld_args* args, bld_data* data) {
@@ -33,8 +34,26 @@ bld_command command_parse(bld_args* args, bld_data* data) {
         cmd.type = BLD_COMMAND_STATUS;
         error = command_status_parse(args, data, &cmd.as.status, &invalid);
     } else if (set_has(&data->targets, string_hash(string_unpack(&base_command)))) {
-        cmd.type = BLD_COMMAND_BUILD;
-        error = command_build_parse(&base_command, args, data, &cmd.as.build, &invalid);
+        bld_path target_config = path_copy(&data->root);
+        path_append_string(&target_config, ".bld");
+        path_append_string(&target_config, "target");
+        path_append_string(&target_config, string_unpack(&base_command));
+        path_append_string(&target_config, "config.json");
+        data->target_config_parsed = !parse_config_target(&target_config, &data->target_config);
+        path_free(&target_config);
+
+        if (!data->target_config_parsed) {
+            error_msg = string_new();
+            string_append_string(&error_msg, "bld: could not parse config file of target '");
+            string_append_string(&error_msg, string_unpack(&base_command));
+            string_append_string(&error_msg, "'");
+            error = 1;
+            invalid.code = -1;
+            invalid.msg = error_msg;
+        } else {
+            cmd.type = BLD_COMMAND_BUILD;
+            error = command_build_parse(&base_command, args, data, &cmd.as.build, &invalid);
+        }
     } else {
         error = 1;
         error_msg = string_pack("bld: '");
