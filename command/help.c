51b4e1cb6f1d551e5c8e93ecb050650b50e868a7
p-rosit <pontus.rosqvist@gmail.com>
2024-05-09 12:50:58 +0200

    print all commands in help

--- command/help.c
+++ command/help.c
@@ -1,6 +1,17 @@
 #include "../bld_core/logging.h"
 #include "help.h"
 
+#include "add.h"
+#include "build.h"
+#include "compiler.h"
+#include "ignore.h"
+#include "init.h"
+#include "invalidate.h"
+#include "linker.h"
+#include "remove.h"
+#include "status.h"
+#include "switch.h"
+
 const bld_string bld_command_string_help = STRING_COMPILE_TIME_PACK("help");
 
 int command_help(bld_command_help* help, bld_data* data) {
@@ -9,6 +20,15 @@ int command_help(bld_command_help* help, bld_data* data) {
         log_info("'%s' help is on the way", string_unpack(&help->command));
     } else {
         log_info("general help is on the way");
+        log_info("  %s", string_unpack(&bld_command_string_add));
+        log_info("  %s", string_unpack(&bld_command_string_compiler));
+        log_info("  %s", string_unpack(&bld_command_string_ignore));
+        log_info("  %s", string_unpack(&bld_command_string_init));
+        log_info("  %s", string_unpack(&bld_command_string_invalidate));
+        log_info("  %s", string_unpack(&bld_command_string_linker));
+        log_info("  %s", string_unpack(&bld_command_string_remove));
+        log_info("  %s", string_unpack(&bld_command_string_status));
+        log_info("  %s", string_unpack(&bld_command_string_switch));
     }
     return 0;
 }
