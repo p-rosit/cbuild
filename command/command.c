101e26cb08de6a2feb8138851f55e3bf943f44a0
p-rosit <pontus.rosqvist@gmail.com>
2024-05-09 12:51:48 +0200

    expose new commands and use implicit targets

--- command/command.c
+++ command/command.c
@@ -2,6 +2,8 @@
 #include "../config/config_target.h"
 #include "command.h"
 
+int command_implicit_target_parse(bld_string*, bld_args*, bld_data*, bld_command*, bld_command_invalid*);
+
 bld_command command_parse(bld_args* args, bld_data* data) {
     int error;
     bld_string base_command;
@@ -24,46 +26,23 @@ bld_command command_parse(bld_args* args, bld_data* data) {
     } else if (!data->has_root) {
         error = 1;
         error_msg = string_new();
-        string_append_string(&error_msg, "bld: not a bld project");
+        string_append_string(&error_msg, "bld: not in a bld project");
         invalid.code = -1;
         invalid.msg = error_msg;
+    } else if (string_eq(&base_command, &bld_command_string_switch)) {
+        cmd.type = BLD_COMMAND_SWITCH;
+        error = command_switch_parse(args, data, &cmd.as.cmd_switch, &invalid);
     } else if (string_eq(&base_command, &bld_command_string_remove)) {
         cmd.type = BLD_COMMAND_REMOVE;
         error = command_remove_parse(args, data, &cmd.as.remove, &invalid);
-    } else if (string_eq(&base_command, &bld_command_string_status)) {
-        cmd.type = BLD_COMMAND_STATUS;
-        error = command_status_parse(args, data, &cmd.as.status, &invalid);
     } else if (string_eq(&base_command, &bld_command_string_help)) {
         cmd.type = BLD_COMMAND_HELP;
         error = command_help_parse(args, data, &cmd.as.help, &invalid);
-    } else if (set_has(&data->targets, string_hash(string_unpack(&base_command)))) {
-        bld_path target_config = path_copy(&data->root);
-        path_append_string(&target_config, ".bld");
-        path_append_string(&target_config, "target");
-        path_append_string(&target_config, string_unpack(&base_command));
-        path_append_string(&target_config, "config.json");
-        data->target_config_parsed = !parse_config_target(&target_config, &data->target_config);
-        path_free(&target_config);
-
-        if (!data->target_config_parsed) {
-            error_msg = string_new();
-            string_append_string(&error_msg, "bld: could not parse config file of target '");
-            string_append_string(&error_msg, string_unpack(&base_command));
-            string_append_string(&error_msg, "'");
-            error = 1;
-            invalid.code = -1;
-            invalid.msg = error_msg;
-        } else {
-            cmd.type = BLD_COMMAND_BUILD;
-            error = command_build_parse(&base_command, args, data, &cmd.as.build, &invalid);
-        }
+    } else if (string_eq(&base_command, &bld_command_string_status)) {
+        cmd.type = BLD_COMMAND_STATUS;
+        error = command_status_parse(NULL, args, data, &cmd.as.status, &invalid);
     } else {
-        error = 1;
-        error_msg = string_pack("bld: '");
-        error_msg = string_copy(&error_msg);
-        string_append_string(&error_msg, string_unpack(&base_command));
-        string_append_string(&error_msg, "' is not a bld command or target");
-        invalid = command_invalid_new(-1, &error_msg);
+        error = command_implicit_target_parse(&base_command, args, data, &cmd, &invalid);
     }
 
     if (error) {
@@ -74,20 +53,125 @@ bld_command command_parse(bld_args* args, bld_data* data) {
     return cmd;
 }
 
+int command_implicit_target_parse(bld_string* base_command, bld_args* args, bld_data* data, bld_command* cmd, bld_command_invalid* invalid) {
+    int build_command = 0;
+    bld_string error_msg;
+    bld_string* target;
+    bld_string next_command;
+
+    if (set_has(&data->targets, string_hash(string_unpack(base_command)))) {
+        target = base_command;
+        if (!args_empty(args)) {
+            next_command = args_advance(args);
+        } else {
+            build_command = 1;
+        }
+    } else {
+        if (!data->config.default_target_configured) {
+            error_msg = string_pack("bld: no default target");
+            invalid->code = -1;
+            invalid->msg = string_copy(&error_msg);
+            return -1;
+        }
+
+        target = &data->config.target;
+        next_command = *base_command;
+    }
+
+    {
+        bld_path target_config = path_copy(&data->root);
+        path_append_string(&target_config, ".bld");
+        path_append_string(&target_config, "target");
+        path_append_string(&target_config, string_unpack(target));
+        path_append_string(&target_config, "config.json");
+        data->target_config_parsed = !parse_config_target(&target_config, &data->target_config);
+        path_free(&target_config);
+
+        if (!data->target_config_parsed) {
+            error_msg = string_new();
+            string_append_string(&error_msg, "bld: could not parse config file of target '");
+            string_append_string(&error_msg, string_unpack(target));
+            string_append_string(&error_msg, "'");
+            invalid->code = -1;
+            invalid->msg = error_msg;
+            return -1;
+        }
+    }
+
+    if (build_command) {
+        cmd->type = BLD_COMMAND_BUILD;
+        return command_build_parse(target, args, data, &cmd->as.build, invalid);
+    } else if (
+        string_eq(&next_command, &bld_command_string_init)
+        || string_eq(&next_command, &bld_command_string_switch)
+        || string_eq(&next_command, &bld_command_string_remove)
+        || string_eq(&next_command, &bld_command_string_help)
+        || string_eq(&next_command, &bld_command_string_status)
+    ) {
+        error_msg = string_new();
+        string_append_string(&error_msg, "bld: command '");
+        string_append_string(&error_msg, string_unpack(&next_command));
+        string_append_string(&error_msg, "' does not take initial target argument");
+        invalid->code = -1;
+        invalid->msg = error_msg;
+        return -1;
+    } else if (string_eq(&next_command, &bld_command_string_add)) {
+        cmd->type = BLD_COMMAND_ADD;
+        return command_add_parse(target, args, data, &cmd->as.add, invalid);
+    } else if (string_eq(&next_command, &bld_command_string_compiler)) {
+        cmd->type = BLD_COMMAND_COMPILER;
+        return command_compiler_parse(target, args, data, &cmd->as.compiler, invalid);
+    } else if (string_eq(&next_command, &bld_command_string_ignore)) {
+        cmd->type = BLD_COMMAND_IGNORE;
+        return command_ignore_parse(target, args, data, &cmd->as.ignore, invalid);
+    } else if (string_eq(&next_command, &bld_command_string_invalidate)) {
+        cmd->type = BLD_COMMAND_INVALIDATE;
+        return command_invalidate_parse(target, args, data, &cmd->as.invalidate, invalid);
+    } else if (string_eq(&next_command, &bld_command_string_linker)) {
+        cmd->type = BLD_COMMAND_LINKER;
+        return command_linker_parse(target, args, data, &cmd->as.linker, invalid);
+    } else if (string_eq(&next_command, &bld_command_string_status)) {
+        cmd->type = BLD_COMMAND_STATUS;
+        return command_status_parse(target, args, data, &cmd->as.status, invalid);
+    } else {
+        error_msg = string_pack("bld: '");
+        error_msg = string_copy(&error_msg);
+        string_append_string(&error_msg, string_unpack(base_command));
+        string_append_string(&error_msg, "' is not a bld command or target");
+        *invalid = command_invalid_new(-1, &error_msg);
+        return -1;
+    }
+
+    log_fatal("command_implicit_target_parse: unreachable error");
+    return -1;
+}
+
 int command_execute(bld_command* cmd, bld_data* data) {
     switch (cmd->type) {
         case (BLD_COMMAND_INVALID):
             return command_invalid(&cmd->as.invalid, data);
         case (BLD_COMMAND_INIT):
             return command_init(&cmd->as.init, data);
+        case (BLD_COMMAND_SWITCH):
+            return command_switch(&cmd->as.cmd_switch, data);
         case (BLD_COMMAND_REMOVE):
             return command_remove(&cmd->as.remove, data);
-        case (BLD_COMMAND_BUILD):
-            return command_build(&cmd->as.build, data);
-        case (BLD_COMMAND_STATUS):
-            return command_status(&cmd->as.status, data);
         case (BLD_COMMAND_HELP):
             return command_help(&cmd->as.help, data);
+        case (BLD_COMMAND_ADD):
+            return command_add(&cmd->as.add, data);
+        case (BLD_COMMAND_BUILD):
+            return command_build(&cmd->as.build, data);
+        case (BLD_COMMAND_COMPILER):
+            return command_compiler(&cmd->as.compiler, data);
+        case (BLD_COMMAND_IGNORE):
+            return command_ignore(&cmd->as.ignore, data);
+        case (BLD_COMMAND_INVALIDATE):
+            return command_invalidate(&cmd->as.invalidate, data);
+        case (BLD_COMMAND_LINKER):
+            return command_linker(&cmd->as.linker, data);
+        case (BLD_COMMAND_STATUS):
+            return command_status(&cmd->as.status, data);
         default:
             log_fatal("command_execute: Unreachable error");
             return -1;
@@ -102,17 +186,35 @@ void command_free(bld_command* cmd) {
         case (BLD_COMMAND_INIT):
             command_init_free(&cmd->as.init);
             return;
+        case (BLD_COMMAND_SWITCH):
+            command_switch_free(&cmd->as.cmd_switch);
+            return;
         case (BLD_COMMAND_REMOVE):
             command_remove_free(&cmd->as.remove);
+            return;
+        case (BLD_COMMAND_HELP):
+            command_help_free(&cmd->as.help);
+            return;
+        case (BLD_COMMAND_ADD):
+            command_add_free(&cmd->as.add);
             return;
         case (BLD_COMMAND_BUILD):
             command_build_free(&cmd->as.build);
             return;
+        case (BLD_COMMAND_COMPILER):
+            command_compiler_free(&cmd->as.compiler);
+            return;
+        case (BLD_COMMAND_IGNORE):
+            command_ignore_free(&cmd->as.ignore);
+            return;
+        case (BLD_COMMAND_INVALIDATE):
+            command_invalidate_free(&cmd->as.invalidate);
+            return;
+        case (BLD_COMMAND_LINKER):
+            command_linker_free(&cmd->as.linker);
+            return;
         case (BLD_COMMAND_STATUS):
             command_status_free(&cmd->as.status);
-            return;
-        case (BLD_COMMAND_HELP):
-            command_help_free(&cmd->as.help);
             return;
         default:
             log_fatal("command_free: Unreachable error");
--- command/command.h
+++ command/command.h
@@ -1,29 +1,49 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 #include "../bld_core/args.h"
-#include "invalid.h"
-#include "init.h"
-#include "remove.h"
+#include "add.h"
 #include "build.h"
-#include "status.h"
+#include "compiler.h"
 #include "help.h"
+#include "ignore.h"
+#include "init.h"
+#include "invalid.h"
+#include "invalidate.h"
+#include "linker.h"
+#include "remove.h"
+#include "status.h"
+#include "switch.h"
 
 typedef enum bld_command_type {
     BLD_COMMAND_INVALID,
+
     BLD_COMMAND_INIT,
+    BLD_COMMAND_SWITCH,
     BLD_COMMAND_REMOVE,
+    BLD_COMMAND_HELP,
+
+    BLD_COMMAND_ADD,
     BLD_COMMAND_BUILD,
-    BLD_COMMAND_STATUS,
-    BLD_COMMAND_HELP
+    BLD_COMMAND_COMPILER,
+    BLD_COMMAND_IGNORE,
+    BLD_COMMAND_INVALIDATE,
+    BLD_COMMAND_LINKER,
+    BLD_COMMAND_STATUS
 } bld_command_type;
 
 typedef union bld_union_command {
     bld_command_invalid invalid;
     bld_command_init init;
+    bld_command_switch cmd_switch;
     bld_command_remove remove;
-    bld_command_build build;
-    bld_command_status status;
     bld_command_help help;
+    bld_command_add add;
+    bld_command_build build;
+    bld_command_compiler compiler;
+    bld_command_ignore ignore;
+    bld_command_invalidate invalidate;
+    bld_command_linker linker;
+    bld_command_status status;
 } bld_union_command;
 
 typedef struct bld_command {
