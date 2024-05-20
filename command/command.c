1a6e636509cd410d8b04a806953655fa6ca012a7
p-rosit <pontus.rosqvist@gmail.com>
2024-05-20 15:38:30 +0200

    move annotated handle to utils

--- command/command.c
+++ command/command.c
@@ -4,47 +4,6 @@
 #include "command.h"
 #include "handle.h"
 
-typedef int (bld_command_convert)(bld_command*, bld_data*, void*, bld_command_invalid*);
-typedef int (bld_command_execute)(void*, bld_data*);
-typedef void (bld_command_free)(void*);
-
-typedef struct bld_handle_named {
-    bld_command_type type;
-    bld_handle handle;
-    bld_command_convert* convert;
-    bld_command_execute* execute;
-    bld_command_free* free;
-} bld_handle_named;
-
-bld_set application_available_set(char* name) {
-    bld_set cmds = set_new(sizeof(bld_handle_named));
-    bld_handle_named handle;
-
-    handle.type = BLD_COMMAND_ADD;
-    handle.handle = command_handle_add(name);
-    handle.convert = (bld_command_convert*) command_add_convert;
-    handle.execute = (bld_command_execute*) command_add;
-    handle.free = (bld_command_free*) command_add_free;
-    set_add(&cmds, BLD_COMMAND_ADD, &handle);
-
-    handle.type = BLD_COMMAND_INVALID;
-    handle.execute = (bld_command_execute*) command_invalid;
-    handle.free = (bld_command_free*) command_invalid_free;
-    set_add(&cmds, BLD_COMMAND_INVALID, &handle);
-
-    return cmds;
-}
-
-void application_available_free(bld_set* cmds) {
-    bld_iter iter = iter_set(cmds);
-    bld_handle_named* handle;
-    while (iter_next(&iter, (void**) &handle)) {
-        if (handle->type == BLD_COMMAND_INVALID) {continue;}
-        handle_free(&handle->handle);
-    }
-    set_free(cmds);
-}
-
 bld_application_command application_command_parse(bld_args* args, bld_data* data, bld_set* handles) {
     int error;
     bld_array errs;
--- command/utils.h
+++ command/utils.h
@@ -5,6 +5,7 @@
 #include "../bld_core/path.h"
 #include "../config/config.h"
 #include "../config/config_target.h"
+#include "handle.h"
 
 extern const bld_string bld_path_build;
 extern const bld_string bld_path_target;
@@ -17,7 +18,35 @@ typedef struct bld_data {
     bld_config config;
     int target_config_parsed;
     bld_config_target target_config;
+    bld_set handles;
 } bld_data;
+
+typedef int (bld_command_convert)(bld_command*, bld_data*, void*, void*);
+typedef int (bld_command_execute)(void*, bld_data*);
+typedef void (bld_command_free)(void*);
+
+typedef enum bld_command_type {
+    BLD_COMMAND_INVALID,
+    BLD_COMMAND_INIT,
+    BLD_COMMAND_SWITCH,
+    BLD_COMMAND_REMOVE,
+    BLD_COMMAND_HELP,
+    BLD_COMMAND_ADD,
+    BLD_COMMAND_BUILD,
+    BLD_COMMAND_COMPILER,
+    BLD_COMMAND_IGNORE,
+    BLD_COMMAND_INVALIDATE,
+    BLD_COMMAND_LINKER,
+    BLD_COMMAND_STATUS
+} bld_command_type;
+
+typedef struct bld_handle_annotated {
+    bld_command_type type;
+    bld_handle handle;
+    bld_command_convert* convert;
+    bld_command_execute* execute;
+    bld_command_free* free;
+} bld_handle_annotated;
 
 int         config_load(bld_data*, bld_config*);
 void        config_save(bld_data*, bld_config*);
