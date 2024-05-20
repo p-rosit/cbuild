a005f75bd8aec55a06c40e4d32a18e8aa2bc14da
p-rosit <pontus.rosqvist@gmail.com>
2024-05-20 14:01:04 +0200

    use new command api

--- main.c
+++ main.c
@@ -7,16 +7,22 @@ int main(int argc, char** argv) {
     int result;
     bld_args args;
     bld_data data;
-    bld_command cmd;
+    bld_application_command cmd;
+    bld_set cmds;
 
     args = args_new(argc, argv);
+    if (args_empty(&args)) {log_fatal("main: unreachable error");}
     args_advance(&args); /* Ignore how program was invoked */
 
-    data = data_extract();
-    cmd = command_parse(&args, &data);
-    result = command_execute(&cmd, &data);
+    cmds = application_available_set("bld");
+    if (cmds.size <= 0) {log_fatal("No commands registered!");}
 
-    command_free(&cmd);
+    data = data_extract();
+    cmd = application_command_parse(&args, &data, &cmds);
+    result = application_command_execute(&cmd, &data, &cmds);
+
+    application_command_free(&cmd, &cmds);
     data_free(&data);
+    application_available_free(&cmds);
     return result;
 }
