cbbe8845188c9fcf0c2442d9b995deea0f9ae007
p-rosit <pontus.rosqvist@gmail.com>
2024-05-22 21:27:46 +0200

    declarative invalidate command

--- command/invalidate.h
+++ command/invalidate.h
@@ -2,16 +2,18 @@
 #define COMMAND_INVALIDATE_H
 #include "../bld_core/dstr.h"
 #include "../bld_core/args.h"
+#include "handle.h"
 #include "invalid.h"
 
 extern const bld_string bld_command_string_invalidate;
 
 typedef struct bld_command_invalidate {
     bld_string target;
-    bld_path path;
+    bld_array paths;
 } bld_command_invalidate;
 
-int command_invalidate_parse(bld_string*, bld_args*, bld_data*, bld_command_invalidate*, bld_command_invalid*);
+bld_handle_annotated command_handle_invalidate(char*);
+int command_invalidate_convert(bld_command*, bld_data*, bld_command_invalidate*, bld_command_invalid*);
 int command_invalidate(bld_command_invalidate*, bld_data*);
 void command_invalidate_free(bld_command_invalidate*);
 
