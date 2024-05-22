482cc008b2da2d286dd17550250f9e93c96ac4ac
p-rosit <pontus.rosqvist@gmail.com>
2024-05-22 19:47:48 +0200

    register init command

--- command/utils.c
+++ command/utils.c
@@ -4,6 +4,7 @@
 #include "utils.h"
 #include "add.h"
 #include "ignore.h"
+#include "init.h"
 #include "help.h"
 #include "status.h"
 #include "build.h"
@@ -90,6 +91,7 @@ bld_data data_extract(char* name) {
     data_add_handle(&data, command_handle_add(name));
     data_add_handle(&data, command_handle_ignore(name));
     data_add_handle(&data, command_handle_status(name));
+    data_add_handle(&data, command_handle_init(name));
     data_add_handle(&data, command_handle_build(name));
     data_add_handle(&data, command_handle_invalid(name));
 
