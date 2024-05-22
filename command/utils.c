0c64e9c703dfe69abeef419b9320305fc8875fc9
p-rosit <pontus.rosqvist@gmail.com>
2024-05-22 21:28:15 +0200

    register invalidate command

--- command/utils.c
+++ command/utils.c
@@ -7,6 +7,7 @@
 #include "init.h"
 #include "help.h"
 #include "switch.h"
+#include "invalidate.h"
 #include "remove.h"
 #include "status.h"
 #include "build.h"
@@ -94,6 +95,7 @@ bld_data data_extract(char* name) {
     data_add_handle(&data, command_handle_ignore(name));
     data_add_handle(&data, command_handle_switch(name));
     data_add_handle(&data, command_handle_remove(name));
+    data_add_handle(&data, command_handle_invalidate(name));
     data_add_handle(&data, command_handle_status(name));
     data_add_handle(&data, command_handle_init(name));
     data_add_handle(&data, command_handle_build(name));
