c91cbb9f3082de207382687d5b178f08c17d6bf0
p-rosit <pontus.rosqvist@gmail.com>
2024-05-22 08:10:46 +0200

    ignore command same as add command

--- command/ignore.c
+++ command/ignore.c
@@ -4,124 +4,183 @@
 #include "ignore.h"
 
 const bld_string bld_command_string_ignore = STRING_COMPILE_TIME_PACK("ignore");
-const bld_string bld_command_string_ignore_terse_delete = STRING_COMPILE_TIME_PACK("-d");
-const bld_string bld_command_string_ignore_flag_delete = STRING_COMPILE_TIME_PACK("--delete");
+const bld_string bld_command_string_ignore_flag_delete = STRING_COMPILE_TIME_PACK("delete");
 
 int command_ignore(bld_command_ignore* cmd, bld_data* data) {
     bld_iter iter;
     bld_config_target* config = &data->target_config;
     bld_path path, *temp;
     bld_set ignored_files = set_new(sizeof(bld_path));
-    uintmax_t ignore_id;
+    uintmax_t added_id;
+
+    data->target_config_parsed = !config_target_load(data, &cmd->target, &data->target_config);
     if (!data->target_config_parsed) {
         log_fatal("Could not parse config of target \"%s\"", string_unpack(&cmd->target));
     }
+    log_info("Chosen target: \"%s\"", string_unpack(&cmd->target));
 
     iter = iter_array(&config->ignore_paths);
     while (iter_next(&iter, (void**) &temp)) {
-        ignore_id = os_info_id(path_to_string(temp));
-        if (ignore_id == BLD_INVALID_IDENITIFIER) {continue;}
-        set_add(&ignored_files, ignore_id, temp);
+        added_id = os_info_id(path_to_string(temp));
+        if (added_id == BLD_INVALID_IDENITIFIER) {continue;}
+        set_add(&ignored_files, added_id, temp);
     }
 
-    ignore_id = os_info_id(path_to_string(&cmd->path));
+    iter = iter_array(&cmd->paths);
+    while (iter_next(&iter, (void**) &temp)) {
+        added_id = os_info_id(path_to_string(temp));
 
-    if (ignore_id == BLD_INVALID_IDENITIFIER) {
-        set_free(&ignored_files);
-        log_error("Attempting to ignore '%s' which is not a valid path to a file/directory", path_to_string(&cmd->path));
-        return -1;
-    }
-
-    if (cmd->remove_flag) {
-        bld_iter iter;
-        bld_string a;
-        bld_path *path, *temp;
-        size_t index;
-
-        path = set_get(&ignored_files, ignore_id);
-        if (path == NULL) {
+        if (added_id == BLD_INVALID_IDENITIFIER) {
             set_free(&ignored_files);
-            log_error("Attempting to stop ignoring '%s' which is not ignored by target '%s'", path_to_string(&cmd->path), string_unpack(&cmd->target));
+            log_error("Attempting to ignore '%s' which is not a valid path to a file/directory", path_to_string(temp));
             return -1;
         }
 
-        index = 0;
-        a = string_pack(path_to_string(path));
-        iter = iter_array(&data->target_config.ignore_paths);
-        while (iter_next(&iter, (void**) &temp)) {
-            bld_string b = string_pack(path_to_string(temp));
-            if (string_eq(&a, &b)) {
-                break;
+        if (!cmd->remove_flag) {
+            if (set_has(&ignored_files, added_id)) {
+                set_free(&ignored_files);
+                log_error("Attempting to ignore '%s' is already being ignored by '%s'", path_to_string(temp), string_unpack(&cmd->target));
+                return -1;
             }
-            index += 1;
+
+            log_info("Ignoring: \"%s\"", path_to_string(temp));
+            path = path_copy(temp);
+            array_push(&data->target_config.ignore_paths, &path);
+        } else {
+            bld_iter iter;
+            bld_string a;
+            bld_path *path, *p;
+            size_t index;
+
+            path = set_get(&ignored_files, added_id);
+            if (path == NULL) {
+                set_free(&ignored_files);
+                log_error("Attempting to remove '%s' which is not being ignored by target '%s'", path_to_string(temp), string_unpack(&cmd->target));
+                return -1;
+            }
+
+            index = 0;
+            a = string_pack(path_to_string(path));
+            iter = iter_array(&data->target_config.ignore_paths);
+            while (iter_next(&iter, (void**) &p)) {
+                bld_string b = string_pack(path_to_string(p));
+                if (string_eq(&a, &b)) {break;}
+                index += 1;
+            }
+
+            path = array_get(&data->target_config.ignore_paths, index);
+            path_free(path);
+            array_remove(&data->target_config.ignore_paths, index);
+
+            log_info("Removed: \"%s\"", path_to_string(temp));
         }
-
-        path = array_get(&data->target_config.ignore_paths, index);
-        path_free(path);
-        array_remove(&data->target_config.ignore_paths, index);
-
-        log_info("No longer ignoring: \"%s\" for target \"%s\"", path_to_string(&cmd->path), string_unpack(&cmd->target));
-    } else {
-        if (set_has(&ignored_files, ignore_id)) {
-            set_free(&ignored_files);
-            log_error("Attempting to ignore '%s' which is already being ignored by target '%s'", path_to_string(&cmd->path), string_unpack(&cmd->target));
-            return -1;
-        }
-
-        log_info("Ignoring: \"%s\" for target \"%s\"", path_to_string(&cmd->path), string_unpack(&cmd->target));
-
-        path = path_copy(&cmd->path);
-        array_push(&config->ignore_paths, &path);
     }
 
     config_target_save(data, &cmd->target, config);
-
     set_free(&ignored_files);
     return 0;
 }
 
-int command_ignore_parse(bld_string* target, bld_args* args, bld_data* data, bld_command_ignore* cmd, bld_command_invalid* invalid) {
-    bld_string path_add, error_msg;
-    (void)(data);
+int command_ignore_convert(bld_command* pre_cmd, bld_data* data, bld_command_ignore* cmd, bld_command_invalid* invalid) {
+    int error;
+    bld_iter iter;
+    bld_string err;
+    bld_string *str_path;
+    bld_path path;
+    bld_command_positional* arg;
+    bld_command_positional_optional* opt;
+    bld_command_positional_vargs* varg;
 
-    if (args_empty(args)) {
-        error_msg = string_pack("bld: missing path");
-        invalid->code = -1;
-        invalid->msg = string_copy(&error_msg);
-        return -1;
-    }
+    arg = array_get(&pre_cmd->positional, 0);
+    if (arg->type != BLD_HANDLE_POSITIONAL_OPTIONAL) {log_fatal("command_ignore_convert: missing first optional");}
+    opt = &arg->as.opt;
 
-    path_add = args_advance(args);
-
-    if (args_empty(args)) {
-        cmd->remove_flag = 0;
-    } else {
-        bld_string temp = args_advance(args);
-        cmd->remove_flag = 1;
-
-        if (!string_eq(&path_add, &bld_command_string_ignore_terse_delete) && !string_eq(&path_add, &bld_command_string_ignore_flag_delete)) {
-            error_msg = string_pack("bld: invalid arguments");
-            invalid->code = -1;
-            invalid->msg = string_copy(&error_msg);
-            return -1;
+    error = -1;
+    if (opt->present) {
+        if (!set_has(&data->targets, string_hash(string_unpack(&opt->value)))) {
+            err = string_new();
+            string_append_string(&err, string_unpack(&opt->value));
+            string_append_string(&err, " is not a known target\n");
+            goto parse_failed;
         }
-
-        path_add = temp;
+        cmd->target = string_copy(&opt->value);
+    } else if (data->config_parsed) {
+        if (data->config.default_target_configured) {
+            if (!set_has(&data->targets, string_hash(string_unpack(&data->config.target)))) {
+                err = string_new();
+                string_append_string(&err, "config has default target ");
+                string_append_string(&err, string_unpack(&data->config.target));
+                string_append_string(&err, " which is not a known target\n");
+                goto parse_failed;
+            }
+            cmd->target = string_copy(&data->config.target);
+        } else {
+            err = string_pack("no target specified and no default target set up\n");
+            err = string_copy(&err);
+            goto parse_failed;
+        }
+    } else {
+        err = string_pack("no target specified and no config has been parsed\n");
+        err = string_copy(&err);
+        goto parse_failed;
     }
 
-    if (!args_empty(args)) {
-        error_msg = string_pack("bld: too many inputs");
-        invalid->code = -1;
-        invalid->msg = string_copy(&error_msg);
-        return -1;
+    arg = array_get(&pre_cmd->positional, 2);
+    if (arg->type != BLD_HANDLE_POSITIONAL_VARGS) {log_fatal("command_ignore_convert: no varg");}
+    varg = &arg->as.vargs;
+
+    if (varg->values.size <= 0) {
+        err = string_pack("expected paths to ignore\n");
+        err = string_copy(&err);
+        *invalid = command_invalid_new(-1, &err);
+        goto free_target;
     }
 
-    cmd->target = string_copy(target);
-    cmd->path = path_from_string(string_unpack(&path_add));
+    cmd->paths = array_new(sizeof(bld_path));
+    iter = iter_array(&varg->values);
+    while (iter_next(&iter, (void**) &str_path)) {
+        path = path_from_string(string_unpack(str_path));
+        array_push(&cmd->paths, &path);
+    }
+
+    cmd->remove_flag = set_has(&pre_cmd->flags, string_hash(string_unpack(&bld_command_string_ignore_flag_delete)));
+
     return 0;
+    free_target:
+    string_free(&cmd->target);
+    parse_failed:
+    *invalid = command_invalid_new(error, &err);
+    return -1;
+}
+
+bld_handle_annotated command_handle_ignore(char* name) {
+    bld_handle_annotated handle;
+
+    handle.type = BLD_COMMAND_IGNORE;
+    handle.handle = handle_new(name);
+    handle_positional_optional(&handle.handle, "The target to modify.");
+    handle_positional_expect(&handle.handle, string_unpack(&bld_command_string_ignore));
+    handle_allow_flags(&handle.handle);
+    handle_positional_vargs(&handle.handle, "The paths to ignore on the chosen the target");
+    handle_flag(&handle.handle, *bld_command_string_ignore_flag_delete.chars, string_unpack(&bld_command_string_ignore_flag_delete), "Remove the specified paths from the ignored paths of the target");
+    handle_set_description(&handle.handle, "Ignores the specified paths in the specified target. If no target is supplied\nthe paths will be ignored by the currently active target.");
+
+    handle.convert = (bld_command_convert*) command_ignore_convert;
+    handle.execute = (bld_command_execute*) command_ignore;
+    handle.free = (bld_command_free*) command_ignore_free;
+
+    return handle;
 }
 
 void command_ignore_free(bld_command_ignore* cmd) {
+    bld_iter iter;
+    bld_path* path;
+
     string_free(&cmd->target);
-    path_free(&cmd->path);
+
+    iter = iter_array(&cmd->paths);
+    while (iter_next(&iter, (void**) &path)) {
+        path_free(path);
+    }
+    array_free(&cmd->paths);
 }
