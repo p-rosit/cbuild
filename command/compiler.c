7639c15fa6e1196e27236d78e00f394e512e9761
p-rosit <pontus.rosqvist@gmail.com>
2024-05-24 17:34:57 +0200

    declarative compiler command

--- command/compiler.c
+++ command/compiler.c
@@ -4,43 +4,57 @@
 const bld_string bld_command_string_compiler = STRING_COMPILE_TIME_PACK("compiler");
 
 int command_compiler(bld_command_compiler* cmd, bld_data* data) {
-    if (cmd->has_path) {
-        log_warn("Configure compilers of target \"%s\" under \"%s\"", string_unpack(&cmd->target), path_to_string(&cmd->path));
-    } else {
-        log_warn("Configure compilers of target \"%s\"", string_unpack(&cmd->target));
-    }
     (void)(data);
+    log_info("Configure compiler of \"%s\"", string_unpack(&cmd->target));
     return -1;
 }
 
-int command_compiler_parse(bld_string* target, bld_args* args, bld_data* data, bld_command_compiler* cmd, bld_command_invalid* invalid) {
-    bld_string path_add, error_msg;
+int command_compiler_convert(bld_command* pre_cmd, bld_data* data, bld_command_compiler* cmd, bld_command_invalid* invalid) {
+    bld_string err;
+    bld_command_positional* arg;
+    bld_command_positional_required* path;
+    bld_command_positional_optional* target;
     (void)(data);
 
-    if (args_empty(args)) {
-        cmd->target = string_copy(target);
-        cmd->has_path = 0;
-        return 0;
+    arg = array_get(&pre_cmd->positional, 0);
+    if (arg->type != BLD_HANDLE_POSITIONAL_OPTIONAL) {log_fatal("command_compiler_convert: missing first optional");}
+    target = &arg->as.opt;
+
+    if (!utils_get_target(&cmd->target, &err, target, data)) {
+        goto parse_failed;
     }
 
-    path_add = args_advance(args);
+    arg = array_get(&pre_cmd->positional, 2);
+    if (arg->type != BLD_HANDLE_POSITIONAL_REQUIRED) {log_fatal("command_compiler_convert: missing required");}
+    path = &arg->as.req;
 
-    if (!args_empty(args)) {
-        error_msg = string_pack("bld: too many inputs");
-        invalid->code = -1;
-        invalid->msg = string_copy(&error_msg);
-        return -1;
-    }
+    cmd->path = path_from_string(string_unpack(&path->value));
 
-    cmd->target = string_copy(target);
-    cmd->has_path = 1;
-    cmd->path = path_from_string(string_unpack(&path_add));
     return 0;
+    parse_failed:
+    *invalid = command_invalid_new(-1, &err);
+    return -1;
+}
+
+bld_handle_annotated command_handle_compiler(char* name) {
+    bld_handle_annotated handle;
+
+    handle.type = BLD_COMMAND_COMPILER;
+    handle.handle = handle_new(name);
+    handle_positional_optional(&handle.handle, "Target to set compiler flags for");
+    handle_positional_expect(&handle.handle, string_unpack(&bld_command_string_compiler));
+    handle_positional_required(&handle.handle, "Directory/File to set flags for");
+    handle_allow_arbitrary_flags(&handle.handle, "Passthrough compiler flags");
+    handle_set_description(&handle.handle, "Set compiler flags");
+
+    handle.convert = (bld_command_convert*) command_compiler_convert;
+    handle.execute = (bld_command_execute*) command_compiler;
+    handle.free = (bld_command_free*) command_compiler_free;
+
+    return handle;
 }
 
 void command_compiler_free(bld_command_compiler* cmd) {
     string_free(&cmd->target);
-    if (cmd->has_path) {
-        path_free(&cmd->path);
-    }
+    path_free(&cmd->path);
 }
--- command/compiler.h
+++ command/compiler.h
@@ -2,17 +2,18 @@
 #define COMMAND_COMPILER_H
 #include "../bld_core/dstr.h"
 #include "../bld_core/args.h"
+#include "handle.h"
 #include "invalid.h"
 
 extern const bld_string bld_command_string_compiler;
 
 typedef struct bld_command_compiler {
     bld_string target;
-    int has_path;
     bld_path path;
 } bld_command_compiler;
 
-int command_compiler_parse(bld_string*, bld_args*, bld_data*, bld_command_compiler*, bld_command_invalid*);
+bld_handle_annotated command_handle_compiler(char*);
+int command_compiler_convert(bld_command*, bld_data*, bld_command_compiler*, bld_command_invalid*);
 int command_compiler(bld_command_compiler*, bld_data*);
 void command_compiler_free(bld_command_compiler*);
 
