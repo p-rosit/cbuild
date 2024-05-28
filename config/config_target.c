69cbe2a05ab071b6f772cef9d0c4cb0c0152aeae
p-rosit <pontus.rosqvist@gmail.com>
2024-05-28 17:56:48 +0200

    parse sub files of build info

--- config/config_target.c
+++ config/config_target.c
@@ -18,6 +18,7 @@ int parse_target_build_info_file_compiler(FILE*, bld_target_build_information*);
 int parse_target_build_info_file_compiler_flags(FILE*, bld_target_build_information*);
 int parse_target_build_info_file_linker_flags(FILE*, bld_target_build_information*);
 int parse_target_build_info_file_sub_files(FILE*, bld_target_build_information*);
+int parse_target_build_info_file_sub_file(FILE*, bld_array*);
 
 bld_config_target config_target_new(bld_path* path) {
     bld_config_target config;
@@ -445,13 +446,34 @@ int parse_target_build_info_file_linker_flags(FILE* file, bld_target_build_infor
 }
 
 int parse_target_build_info_file_sub_files(FILE* file, bld_target_build_information* info) {
-    bld_target_build_information sub_info;
-    int result = parse_target_build_info(file, &sub_info);
-    if (result) {
-        log_warn("could not parse files under ");
+    bld_array sub_files;
+    int amount_parsed;
+
+    sub_files = array_new(sizeof(bld_target_build_information));
+    amount_parsed = json_parse_array(file, &sub_files, (bld_parse_func) parse_target_build_info_file_sub_file);
+
+    if (amount_parsed < 0) {
+        log_warn("could not parse files under \"%s\"", string_unpack(&info->name));
+        log_fatal("Free correctly!");
         return -1;
     }
 
-    array_push(&info->files, &sub_info);
+    array_free(&info->files);
+    info->files = sub_files;
+
+    return 0;
+}
+
+int parse_target_build_info_file_sub_file(FILE* file, bld_array* files) {
+    bld_target_build_information info;
+    int result;
+
+    result = parse_target_build_info(file, &info);
+    if (result) {
+        log_warn("could not parse file");
+        return -1;
+    }
+
+    array_push(files, &info);
     return 0;
 }
