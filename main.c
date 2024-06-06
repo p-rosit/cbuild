0ad28a5cfbdad437c125b09515340bcd7fbc0d3a
p-rosit <pontus.rosqvist@gmail.com>
2024-06-06 07:59:00 +0200

    remove set of commands

--- main.c
+++ main.c
@@ -8,13 +8,11 @@ int main(int argc, char** argv) {
     bld_args args;
     bld_data data;
     bld_application_command cmd;
-    bld_set cmds;
 
     args = args_new(argc, argv);
     if (args_empty(&args)) {log_fatal("main: unreachable error");}
     args_advance(&args); /* Ignore how program was invoked */
 
-    if (cmds.size <= 0) {log_fatal("No commands registered!");}
     data = data_extract("bld");
     cmd = application_command_parse(&args, &data);
     result = application_command_execute(&cmd, &data);
