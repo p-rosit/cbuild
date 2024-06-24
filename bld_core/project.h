5f6c92121a49b243490ee381d9b563a3b47170ff
p-rosit <pontus.rosqvist@gmail.com>
2024-06-24 07:55:58 +0200

    refer to base from build and cache

--- bld_core/project.h
+++ bld_core/project.h
@@ -10,24 +10,30 @@
 
 #define BLD_CACHE_NAME "cache.json"
 
-typedef struct bld_project_cache {
+typedef struct bld_project_cache bld_project_cache;
+typedef struct bld_project_base bld_project_base;
+
+struct bld_project_cache {
     int loaded;
     int set;
     int applied;
+    bld_project_base* base;
     bld_path root;
+    uintmax_t root_file;
     bld_linker linker;
     bld_set files;
-} bld_project_cache;
+};
 
-typedef struct bld_project_base {
+struct bld_project_base {
     int rebuilding;
+    bld_project_base* build_of;
     bld_path root;
     int standalone;
     bld_path build;
     bld_set compiler_handles;
     bld_linker linker;
     bld_project_cache cache;
-} bld_project_base;
+};
 
 typedef struct bld_forward_project {
     int resolved;
