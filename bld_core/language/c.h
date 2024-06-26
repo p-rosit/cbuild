8fa78a8b138a2e25137d4971c80e48deba52731f
p-rosit <pontus.rosqvist@gmail.com>
2024-06-26 20:14:52 +0200

    add c and cpp

---
+++ bld_core/language/c.h
@@ -0,0 +1,9 @@
+#ifndef LANGUAGE_C_H
+#define LANGUAGE_C_H
+#include "language.h"
+
+extern bld_string bld_language_string_c;
+
+int language_get_includes_c(bld_project_base*, bld_path*, bld_file*, bld_set*);
+
+#endif
---
+++ bld_core/language/cpp.h
@@ -0,0 +1,9 @@
+#ifndef LANGUAGE_CPP_H
+#define LANGUAGE_CPP_H
+#include "language.h"
+
+extern bld_string bld_language_string_cpp;
+
+int language_get_includes_cpp(bld_project_base*, bld_path*, bld_file*, bld_set*);
+
+#endif
