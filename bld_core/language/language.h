e67b1459a3fa4bd33ef421ded5059c232ced15dd
p-rosit <pontus.rosqvist@gmail.com>
2024-06-26 20:14:25 +0200

    abstract language interface

---
+++ bld_core/language/language.h
@@ -0,0 +1,22 @@
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+#include "../dstr.h"
+#include "../path.h"
+#include "../array.h"
+#include "../set.h"
+#include "../file.h"
+#include "../project_base.h"
+
+typedef enum bld_language_type {
+    BLD_LANGUAGE_C,
+    BLD_LANGUAGE_CPP,
+    BLD_LANGUAGE_AMOUNT
+} bld_language_type;
+
+bld_array language_get_available(void);
+bld_language_type language_get_mapping(bld_string*);
+bld_string* language_get_string(bld_language_type);
+
+int language_get_includes(bld_language_type, bld_project_base*, bld_path*, bld_file*, bld_set*);
+
+#endif
