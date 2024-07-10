787a9b2792058f21e9cabe987204d75d55a7817d
p-rosit <pontus.rosqvist@gmail.com>
2024-07-10 07:36:07 +0200

    parse cache handle

--- bld_core/cache.c
+++ bld_core/cache.c
@@ -1,4 +1,6 @@
+#include <limits.h>
 #include "logging.h"
+#include "json.h"
 #include "os.h"
 #include "cache.h"
 
@@ -23,6 +25,13 @@ void cache_entry_free(bld_cache_entry*);
 
 void cache_handle_purge(bld_cache_handle*);
 int cache_handle_parse(bld_cache_handle*);
+int parse_cache_handle(FILE*, bld_cache_handle*);
+int parse_cache_handle_files(FILE*, bld_cache_handle*);
+int parse_cache_handle_file(FILE*, bld_array*);
+int parse_cache_handle_file_name(FILE*, bld_cache_entry*);
+int parse_cache_handle_file_builds(FILE*, bld_cache_entry*);
+int parse_cache_handle_file_path(FILE*, bld_cache_entry*);
+int parse_cache_handle_file_mtime(FILE*, bld_cache_entry*);
 
 bld_cache_handle cache_handle_new(bld_project_base* base, bld_path* root) {
     int error;
@@ -76,5 +85,163 @@ void cache_entry_free(bld_cache_entry* entry) {
 }
 
 int cache_handle_parse(bld_cache_handle* cache) {
+    FILE* file;
+    int error;
+    bld_path path;
+
+    path = path_copy(&cache->base->root);
+    path_append_path(&path, &cache->root);
+    path_append_string(&path, "cache.json");
+    file = fopen(path_to_string(&path), "r");
+
+    error = 0;
+    if (file == NULL) {
+        log_debug("No cache file found.");
+    } else {
+        error = parse_cache_handle(file, cache);
+        fclose(file);
+
+        if (error) {
+            log_warn("Could not parse cache, ignoring.");
+        } else {
+            log_dinfo("Loaded cache");
+        }
+    }
+
+    path_free(&path);
+    return error;
+}
+
+int parse_cache_handle(FILE* file, bld_cache_handle* cache) {
+    int size = 1;
+    int parsed[1];
+    char *keys[1] = {"files"};
+    bld_parse_func funcs[1] = {
+        (bld_parse_func) parse_cache_handle_files,
+    };
+
+    json_parse_map(file, cache, size, parsed, keys, funcs);
+    if (!parsed[0]) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int parse_cache_handle_files(FILE* file, bld_cache_handle* cache) {
+    int amount_parsed;
+    bld_array files;
+
+    files = array_new(sizeof(bld_cache_entry));
+    amount_parsed = json_parse_array(file, &files, (bld_parse_func) parse_cache_handle_file);
+    if (amount_parsed < 0) {
+        log_fatal(LOG_FATAL_PREFIX "free correctly");
+        return -1;
+    }
+
+    {
+        bld_iter iter;
+        bld_cache_entry* entry;
+
+        iter = iter_array(&files);
+        while (iter_next(&iter, (void**) &entry)) {
+            bld_path path;
+            bld_file_id id;
+
+            path = path_copy(&cache->base->root);
+            path_append_path(&path, &entry->path);
+
+            id = file_get_id(&path);
+            if (id == BLD_INVALID_IDENITIFIER) {
+                cache_entry_free(entry);
+            } else {
+                set_add(&cache->files, id, entry);
+            }
+
+            path_free(&path);
+        }
+
+        array_free(&files);
+    }
+
+    return 0;
+}
+
+int parse_cache_handle_file(FILE* file, bld_array* entries) {
+    int amount_parsed;
+    int size = 4;
+    int parsed[4];
+    char *keys[4] = {"name", "builds_since_access", "path", "mtime"};
+    bld_parse_func funcs[4] = {
+        (bld_parse_func) parse_cache_handle_file_name,
+        (bld_parse_func) parse_cache_handle_file_builds,
+        (bld_parse_func) parse_cache_handle_file_path,
+        (bld_parse_func) parse_cache_handle_file_mtime,
+    };
+    bld_cache_entry entry;
+
+    amount_parsed = json_parse_map(file, &entry, size, parsed, keys, funcs);
+    if (amount_parsed != size) {
+        log_fatal(LOG_FATAL_PREFIX "free correctly");
+        return -1;
+    }
+
+    array_push(entries, &entry);
+    return 0;
+}
+
+int parse_cache_handle_file_name(FILE* file, bld_cache_entry* entry) {
+    int error;
+    bld_string str;
+
+    error = string_parse(file, &str);
+    if (error) {
+        return -1;
+    }
+
+    entry->dir_name = str;
+    return 0;
+}
+
+int parse_cache_handle_file_builds(FILE* file, bld_cache_entry* entry) {
+    int error;
+    uintmax_t num;
+
+    error = parse_uintmax(file, &num);
+    if (error) {
+        return -1;
+    }
+
+    if (num > INT_MAX) {
+        return -1;
+    }
+    entry->builds_since_access = (int) num;
+    return 0;
+}
+
+int parse_cache_handle_file_path(FILE* file, bld_cache_entry* entry) {
+    int error;
+    bld_string str;
+
+    error = string_parse(file, &str);
+    if (error) {
+        return -1;
+    }
+
+    entry->path = path_from_string(string_unpack(&str));
+    string_free(&str);
+    return 0;
+}
+
+int parse_cache_handle_file_mtime(FILE* file, bld_cache_entry* entry) {
+    int error;
+    uintmax_t num;
+
+    error = parse_uintmax(file, &num);
+    if (error) {
+        return -1;
+    }
+
+    entry->mtime = num;
     return 0;
 }
