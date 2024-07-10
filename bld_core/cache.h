b42d9536a627ab3c61fd8a28dc9ba8efcb89207b
p-rosit <pontus.rosqvist@gmail.com>
2024-07-10 07:37:01 +0200

    expand cache handle functions

--- bld_core/cache.h
+++ bld_core/cache.h
@@ -13,7 +13,10 @@ typedef struct bld_cache_handle {
     bld_set loaded_files;
 } bld_cache_handle;
 
-bld_cache_handle cache_handle_new(bld_path*);
-int cache_object_get(bld_file*, bld_compiler*);
+bld_cache_handle cache_handle_new(bld_project_base*, bld_path*);
+void cache_handle_free(bld_cache_handle*);
+int cache_object_get(bld_cache_handle*, bld_file*, bld_compiler*, bld_path*);
+int cache_includes_get(bld_cache_handle*, bld_file*, bld_compiler*, bld_set*);
+int cache_symbols_get(bld_cache_handle*, bld_file*, bld_compiler*, bld_set*, bld_set*);
 
 #endif
