63bcc05e05e9b644ab85b200ee0a55c56e1148eb
p-rosit <pontus.rosqvist@gmail.com>
2024-07-11 19:12:03 +0200

    resolve dependencies

--- bld_core/incremental.c
+++ bld_core/incremental.c
@@ -652,37 +652,22 @@ int incremental_compile_changed_files(bld_project* project, bld_set* changed_fil
     return result;
 }
 
-int incremental_project_dependencies(bld_project* project, int* any_compiled) {
-    int result;
-    bld_set changed_files;
+int incremental_project_dependencies(bld_project* project) {
+    int error;
 
-    {
-        int temp;
-        bld_iter iter;
-        bld_file* file;
-
-        temp = 0;
-        changed_files = set_new(sizeof(int));
-        iter = iter_set(&project->files);
-        while (iter_next(&iter, (void**) &file)) {
-            set_add(&changed_files, file->identifier.id, &temp);
-        }
+    error = dependency_graph_extract_includes(&project->graph, project);
+    if (error) {
+        log_warn("Includes could not be extracted");
+        return error;
     }
 
-    dependency_graph_extract_includes(&project->graph, project);
-    incremental_mark_changed_files(project, &changed_files);
-
-    *any_compiled = 0;
-    result = incremental_compile_changed_files(project, &changed_files, any_compiled);
-    set_free(&changed_files);
-
-    dependency_graph_extract_symbols(&project->graph, project);
-
-    if (result) {
-        log_warn("Could not compile all files, no executable generated.");
+    error = dependency_graph_extract_symbols(&project->graph, project);
+    if (error) {
+        log_warn("Symbols of project could not be extracted");
+        return error;
     }
 
-    return result;
+    return 0;
 }
 
 int incremental_compile_executable(bld_project* project, char* name) {
