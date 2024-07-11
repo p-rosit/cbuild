a1f05eeeff2e2c90f007227a8e28d3cf541df8f5
p-rosit <pontus.rosqvist@gmail.com>
2024-07-11 19:05:43 +0200

    allow asking if any files were compiled

--- bld_core/cache.h
+++ bld_core/cache.h
@@ -8,6 +8,7 @@
 
 bld_cache_handle cache_handle_new(bld_project_base*, bld_path*);
 void cache_handle_free(bld_cache_handle*);
+int cache_any_compiled(bld_cache_handle*);
 int cache_object_get(bld_cache_handle*, bld_file*, bld_compiler*, bld_path*);
 int cache_includes_get(bld_cache_handle*, bld_file*, bld_compiler*, bld_set*);
 int cache_symbols_get(bld_cache_handle*, bld_file*, bld_compiler*, bld_set*, bld_set*);
